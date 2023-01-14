ae67c4a7b64b6e3e9f807018895cdbece2509e39
MSreeN <khyathimnsree369@gmail.com>
2023-01-14 19:09:19 +0530

    default val and passing ref and value

--- Funcitons/script.js
+++ Funcitons/script.js
@@ -10,5 +10,25 @@ function createBooking(flightNum, numPassengers = 5, price = 1000){
   bookings.push(booking)
 }
 
-createBooking('LH123')
+createBooking('LH123', undefined, 5000)
 console.log(bookings);
+
+const flight = "LH234";
+const jonas = {
+  name: 'Sree',
+  passport: 2345543254353
+}
+
+function checkIn(flightNum, passenger){
+  flightNum = "3434",
+  passenger.name= "Mr. " +passenger.name;
+}
+const b = structuredClone(jonas)
+console.log(checkIn(flight, b));
+console.log(flight);
+console.log(jonas);
+
+
+const newPassport = function(person){
+  
+}
