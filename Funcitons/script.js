6df09fa95347b3d3eb7bb04b123b21e1c8151bbd
MSreeN <khyathimnsree369@gmail.com>
2023-01-16 18:31:29 +0530

    higher order and first class function

--- Funcitons/script.js
+++ Funcitons/script.js
@@ -6,12 +6,12 @@ function createBooking(flightNum, numPassengers = 5, price = 1000){
     numPassengers,
     price
   }
-  console.log(booking);
+  // console.log(booking);
   bookings.push(booking)
 }
 
 createBooking('LH123', undefined, 5000)
-console.log(bookings);
+// console.log(bookings);
 
 const flight = "LH234";
 const jonas = {
@@ -23,12 +23,26 @@ function checkIn(flightNum, passenger){
   flightNum = "3434",
   passenger.name= "Mr. " +passenger.name;
 }
-const b = structuredClone(jonas)
-console.log(checkIn(flight, b));
-console.log(flight);
-console.log(jonas);
+// const b = structuredClone(jonas)
+// console.log(checkIn(flight, b));
+// console.log(flight);
+// console.log(jonas);
 
 
 const newPassport = function(person){
   
 }
+
+//////////First Class and higher order function
+function upperFirstWord(str){
+  const [firstWord, ...restWords] = str.split(" ");
+  return [firstWord.toUpperCase(), ...restWords].join(" ")
+}
+
+function transformer(str, func){
+  console.log(`Transformed string is ${func(str)}`);
+}
+
+// transformer function will call the upperFirstWord function
+transformer("JavaScript is the best!", upperFirstWord)
+//^^ Higher order function--call back function^^^
