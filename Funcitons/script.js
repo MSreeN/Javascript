529090b743497900c71e036824d189f7984bde78
MSreeN <khyathimnsree369@gmail.com>
2023-01-17 19:47:01 +0530

    call and apply method

--- Funcitons/script.js
+++ Funcitons/script.js
@@ -44,5 +44,62 @@ function transformer(str, func){
 }
 
 // transformer function will call the upperFirstWord function
-transformer("JavaScript is the best!", upperFirstWord)
+// transformer("JavaScript is the best!", upperFirstWord)
 //^^ Higher order function--call back function^^^
+
+/////////////functions returning functions
+
+function greet(name){
+  return function(){
+    // console.log(`Hello ${name}`);
+  }
+}
+
+const retFun = greet("sree");
+// retFun();
+// greet("sree")("Hello")
+
+/////////Writing above function using arrow function
+
+const greet1 = name => {
+  return greet => console.log(`${greet} ${name}`);
+}
+
+// const returnFunc = greet1("sree")
+// returnFunc("Hello")
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name){
+    console.log(`${name} booked a seat on ${this.airline}
+     flight ${this.iataCode}${flightNum}`);
+     this.bookings.push({
+      flight: `${this.iataCode}${flightNum}`,name
+     })
+  }
+
+}
+// lufthansa.book(239, "sree")
+// lufthansa.book(635, "khyathi")
+
+const euroWings = {
+  name: 'EuroWings',
+  iataCode: "EW",
+  bookings:[],
+}
+//Call method
+
+
+const book = lufthansa.book;
+//we are calling call method and call will call the 
+//book function with this keyword set to the object
+book.call(lufthansa, 234, "sree")
+book.call(euroWings,234, "sree")
+console.log(lufthansa.bookings);
+console.log(euroWings.bookings);
+
+//////////apply method
+book.apply(lufthansa,['mah', "mah"])
+// console.log(lufthansa.bookings);
