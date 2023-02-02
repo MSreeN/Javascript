bdd1280ad4de676fd3852facecb0bd57b4e83c1e
MSreeN <khyathimnsree369@gmail.com>
2023-02-02 18:23:09 +0530

    challenge updated by chaining methods

--- Array, Strings, Maps/Challenges.js
+++ Array, Strings, Maps/Challenges.js
@@ -251,4 +251,49 @@ function checkDogs(dogsJulia, dogsKate) {
     console.log(`Dog number ${index+1} is an ${value<3 ?'puppy': 'adult'} and is ${value} years old`);
   })
 }
-checkDogs(dogsJulia, [4, 1, 15, 8, 3]);
+// checkDogs(dogsJulia, [4, 1, 15, 8, 3]);
+
+
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's 
+// ages ('ages'), and does the following things in order: 
+// 1. Calculate the dog age in human years using the following formula: if the dog is 
+// <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, 
+// humanAge = 16 + dogAge * 4 
+
+const data1 = [5, 2, 4, 1, 15, 8, 3] 
+const data2 = [16, 6, 10, 5, 6, 1, 4]
+let dogHumanAge = []
+function calcAverageHumanAge(data){
+  dogHumanAge = data.map(age => {
+    if(age<=2) return 2* age
+    else return 16+ age*4;
+  })
+  return dogHumanAge;
+}
+// console.log(calcAverageHumanAge(data2))
+// 2. Exclude all dogs that are less than 18 human years old (which is the same as 
+// keeping dogs that are at least 18 years old) 
+dogHumanAge = dogHumanAge.filter((age) => age>17)
+// console.log(dogHumanAge);
+// 3. Calculate the average human age of all adult dogs (you should already know 
+// from other challenges how we calculate averages 😉) 
+let AvgHumanAgeOfDogs = dogHumanAge.reduce(function(acc, cVal){
+  return acc+cVal;
+},0)/dogHumanAge.length
+// console.log(AvgHumanAgeOfDogs);
+// 4. Run the function for both test datasets 
+// Test data: 
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time 
+// as an arrow function, and using chaining! 
+// Test data: 
+// § Data 1: [5, 2, 4, 1, 15, 8, 3] 
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+function calcAverageHumanAge(data){
+  let arrSize;
+  const totDogAges = data.map(age => age<=2?2*age:16+age*4).filter(age => age>17).reduce((acc, cVal,i,arr) => {
+    arrSize  = arr.length;
+    return acc+cVal;
+  },0)
+  return totDogAges/arrSize;
+}
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
